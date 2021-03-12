3b8d8febe13cb0b8b63691b92c32f9c451c2d02e
Keane <KeaneCarotenuto@gmail.com>
2021-03-12 15:26:07 +1300

    Comments and cleaning

--- Fractals/Source.cpp
+++ Fractals/Source.cpp
@@ -9,46 +9,53 @@
 #define width 256
 #define height 256
 
+//Array of pixels
 auto pixels = new sf::RectangleShape[width][height];
 
-//std::vector<sf::Color> cols = { sf::Color::Color(255,0,0), sf::Color::Color(126,126,0), sf::Color::Color(0,255,0), sf::Color::Color(0,126,126), sf::Color::Color(0,0,255), sf::Color::Color(126,0,126) };
-std::vector<sf::Color> cols = { };
+std::vector<sf::Color> cols = { sf::Color::Color(255,0,0), sf::Color::Color(126,126,0), sf::Color::Color(0,255,0), sf::Color::Color(0,126,126), sf::Color::Color(0,0,255), sf::Color::Color(126,0,126) };
+//std::vector<sf::Color> cols = { };
 
-
+//Scale and Coords
 long double zoom = 2;
 long double xcoord = -0.7611251856176778352;
 long double ycoord = -0.084749953259429494645;
 
+//How many innterations to do (max)
 long double itterations = 100;
 
-//double xcoord = -2;
-//double ycoord = -1;
-
+//Amount of threads to use
 int threads = 1;
 
+//Renders a single pixel
 void rendPixel(int x, int y) {
 
+	//Create point and z
 	std::complex<long double> point(((long double)x - width/2) / width * zoom + xcoord, ((long double)y - height / 2) / height * zoom - ycoord);
 	std::complex<long double> z(0, 0);
 
+	//Make pixel
 	sf::RectangleShape rect;
 	rect.setSize({ 1, 1 });
 	rect.setPosition(x, y);
 
 	long double count = 0;
 
+	//Do the fractal stuff
 	while (std::abs(z) < 20 && count < (itterations)) {
-		z = z * z + point /** std::complex<double>(0, count / 10)*/;
+		z = z * z + point /** std::complex<long double>(0, count / 10)*/;
 		count += 1;
 	}
 
+	//Colour pixel
 	rect.setFillColor(cols[(int)count % cols.size()]);
 	rect.setOutlineColor(sf::Color::Black);
 	rect.setOutlineThickness(0.1f);
 
+	//Save pixel to array
 	pixels[x][y] = rect;
 }
 
+//Renders area, by rendering pixels
 void rendSquare(int x1, int x2, int y1, int y2) {
 	for (int y = y1; y < y2; y++) {
 		for (int x = x1; x < x2; x++) {
@@ -59,12 +66,14 @@ void rendSquare(int x1, int x2, int y1, int y2) {
 
 int main() {
 
+	//decimal precision for cout
 	std::cout.precision(20);
 
 	std::cout << "Booting" << "\n";
 
 	std::system("CLS");
 
+	//Check how many threads and ask user
 	unsigned int n = std::thread::hardware_concurrency();
 	std::cout <<"Concurrent threads supported by your machine: " << n << "\n";
 
@@ -72,6 +81,7 @@ int main() {
 
 	std::cin >> threads;
 
+	//I bad answer, set to 1
 	if ((int)threads <= 0) {
 		threads = 1;
 	}
@@ -81,49 +91,6 @@ int main() {
 	//Make Window
 	sf::RenderWindow window(sf::VideoMode(width, height), "Fractals By Keane Carotenuto");
 
-	////Setting coordinates of mid screen, and the amount to render outwards from that point
-	//float xcoord = 0.0f;
-	//float ycoord = 0.0f;
-	//float scale = 1.5f;
-
-	////from -2 to 2 is 4, so we divide that by the view port size, to get how much we need to increment by for each pixel
-	//for (float y = ycoord - scale; y < ycoord + scale; y += ((2 * scale) / height)) {
-	//	for (float x = xcoord - scale; x < xcoord + scale; x += ((2 * scale) / width)) {
-	//		//Make the point
-	//		std::complex<float> point(x, y);
-	//		//Set z to 0 at first
-	//		std::complex<float> z(0, 0);
-
-	//		//Make the "pixel"
-	//		sf::RectangleShape rect;
-	//		rect.setSize({ 1, 1 });
-	//		//Set it to the right position on screen (move into positives, and scale position by screen size)
-	//		rect.setPosition((x - (xcoord - scale)) * (width / (2 * scale)), (y - (ycoord - scale)) * (height / (2 * scale)));
-
-	//		//Init count
-	//		long double count = 0;
-
-	//		//Repeat until gets out of bounds, or reaches over 100 tries
-	//		while (std::abs(z) < 20 && count < (100)) {
-	//			z = z * z + point /** std::complex<double>(0, count / 10)*/;
-	//			count += 1;
-	//		}
-
-	//		//Set the colour to red, with brightness of how many tries
-	//		rect.setFillColor(sf::Color::Color(count, 0, 0));
-
-	//		//Draw pixel
-	//		window.draw(rect);
-	//	}
-	//}
-	//
-	////Display pixel
-	//window.display();
-
-	////Wait for user before quit
-	//int wait;
-	//std::cin >> wait;
-
 	std::vector<std::thread*> ts;
 	ts.clear();
 
@@ -132,7 +99,8 @@ int main() {
 	bool update = true;
 	bool autoDepth = false;
 
-	int r = 125;
+	//Make cool gradients for colours
+	/*int r = 125;
 	int g = 125;
 	int b = 125;
 
@@ -162,17 +130,18 @@ int main() {
 		else if (b > 125) {
 			b--;
 		}
-	}
+	}*/
 
+	//Main Loop
 	while (window.isOpen() == true)
 	{
-
+		//if needs to be updated, update
 		if (update == true) {
 
+			//Log start time
 			auto start = std::chrono::high_resolution_clock::now();
 
-			
-
+			//Split task into threads
 			for (int y = 0; y < threads; y++) {
 					std::thread* t_thread = new std::thread(rendSquare, 0, width, y * height / threads, (y + 1) * height / threads);
 
@@ -181,57 +150,67 @@ int main() {
 					ts.push_back(t_thread);
 			}
 
+			//Wait for all threads to be done
 			for (std::thread* _t : ts) {
 				_t->join();
 			}
 
+			//Clear threads
 			for (std::thread* _t : ts) {
 				delete _t;
 			}
-
 			ts.clear();
 
-
-
+			//Clear Screen
 			window.clear();
 
+			//Draw pixels
 			for (int y = 0; y < height; y++) {
 				for (int x = 0; x < width; x++) {
 					window.draw(pixels[x][y]);
 				}
 			}
 
+			//Log Finish time
 			auto finish = std::chrono::high_resolution_clock::now();
 
+			//Display
 			window.display();
 			update = false;
 
+			//Calc time taken
+			std::chrono::duration<double> elapsed = finish - start;
+
+			system("CLS");
+
+			//Make sure cursor is at top
 			COORD pos = { 0, 0 };
 			HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
 			SetConsoleCursorPosition(output, pos);
 
-			std::chrono::duration<double> elapsed = finish - start;
-
-			std::cout	<<						   "Scale: " << zoom
-						<< "                      \nTime: " << elapsed.count() 
-						<< "                      \nCoords:  (" << (long double)xcoord << ", " << (long double)ycoord << ")                      " 
-						<< "                      \nItterations: " << itterations << "                      " 
-						<< "                      \nThreads Used: " << threads << "                      ";
+			//Display info
+			std::cout	<< "Scale: " << zoom
+						<< "\nTime: " << elapsed.count() 
+						<< "\nCoords:  (" << (long double)xcoord << ", " << (long double)ycoord << ")" 
+						<< "\nItterations: " << itterations 
+						<< "\nThreads Used: " << threads;
 		}
 
+		//Makes colours go woooo
 		bool spin = false;
-
 		if (spin){
 			cols.push_back(cols[0]);
 			cols.erase(cols.begin());
 			update = true;
 		}
 
+		//Makes the program continuously increase itterations
 		if (autoDepth) {
 			itterations++;
 			update = true;
 		}
 
+		//React to user input, zoom and stuff
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
